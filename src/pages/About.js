0e714b1e30c7f1b4537eb7b63688ed68a9523460
keyvan1996 <keyvanshabani@Keyvans-MacBook-Pro.local>
2021-09-29 19:47:53 -0400

    about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,20 @@ import React from "react";
 function About(){
 
     return(
-        <div>A study conducted by OnePoll, in conjunction with Evite, looked into the birthday celebration habits of 2,000 Americans and found that as many as 35 percent of people have had their significant other forget their birthday.
+        <div class="about-section">
+        <div class="inner-container">
+            <h1>About Us</h1>
+            <p class="text">
+            A study conducted by OnePoll, in conjunction with Evite, looked into the birthday celebration habits of 2,000 Americans and found that as many as 35 percent of people have had their significant other forget their birthday.
         This is where Twish comes into play, a web application that enables people to send automatic celebratory wishes. Including customized messages ready to be sent.
+        
+            </p>
+            <div class="skills">
+                <span>Web Design</span>
+                <span>Coding</span>
+            </div>
         </div>
+    </div>
     )
 }
 
